eea56a8a7a8df43eaa5c5675ec2d0322713c5dc7
ammmar <ammaraslam7164@gmail.com>
2024-01-05 23:36:12 +0600

    border setted

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,17 +1,15 @@
+import "./Bottle.css"
 const Bottle = ({ bottle }) => {
-  const {name, image,price} = bottle;
+  const { name, image, price } = bottle;
 
   console.log(bottle);
 
   return (
     <div>
-    
-
-      <div>
-       
+      <div className="bottle">
         <img src={image} alt="" />
-         <h2>Name:{name}</h2>
-         <h2>price:{price}</h2>
+        <h2>Name:{name}</h2>
+        <h2>price:{price}</h2>
       </div>
     </div>
   );
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
@@ -14,10 +15,12 @@ const Bottles = () => {
     <div>
       <h2>Bottles Here</h2>
       {bottles.length}
-
-      {bottles.map((bottle) => (
-        <Bottle key={bottle.key} bottle={bottle}></Bottle>
-      ))}
+      <div className="bottle-container">
+        {" "}
+        {bottles.map((bottle) => (
+          <Bottle key={bottle.key} bottle={bottle}></Bottle>
+        ))}
+      </div>
     </div>
   );
 };
