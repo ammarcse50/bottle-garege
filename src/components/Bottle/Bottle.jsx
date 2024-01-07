c48cfdc5fd8ee21b2f331f4046b8f558ed4b234f
ammmar <ammaraslam7164@gmail.com>
2024-01-07 11:20:59 +0600

    updated

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,15 +1,15 @@
 import "./Bottle.css"
-const Bottle = ({ bottle }) => {
-  const { name, image, price } = bottle;
+const Bottle = ({ bottle,handleAddToCart }) => {
+  const { name, img, price } = bottle;
 
-  console.log(bottle);
 
   return (
     <div>
       <div className="bottle">
-        <img src={image} alt="" />
+        <img src={img} alt="" />
         <h2>Name:{name}</h2>
         <h2>price:{price}</h2>
+        <button onClick={()=>handleAddToCart(bottle)}>Purchase</button>
       </div>
     </div>
   );
